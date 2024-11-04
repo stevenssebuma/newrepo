d2074a4ba41549e3f85198c794a1fded6793f1ea
stevenssebuma <ssebumasteven4@gmail.com>
2024-11-04 18:07:46 +0300

    ADDED CONTENT

--- server.js
+++ server.js
@@ -5,27 +5,27 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
 
 /* ***********************
  * View Engine
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") //not at view root
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); //not at view root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 //Index route
 app.get("/", function(req, res){
-  res.render("index", {title:"Home"})
-})
+  res.render("index", {title:"Home"});
+});
 
 /* ***********************
  * Local Server Information
@@ -38,5 +38,5 @@ const host = process.env.HOST
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
