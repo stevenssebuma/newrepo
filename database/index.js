5d19ed6e8d5c3d38298dd3d5aa76c03b82112edb
stevenssebuma <ssebumasteven4@gmail.com>
2024-11-27 18:27:30 +0300

    Made changes on DATABASE_URL

--- database/index.js
+++ database/index.js
@@ -1,4 +1,4 @@
-const {Pool} = require("pg")
+const { Pool } = require("pg")
 require("dotenv").config()
 /* ***************
  * Connection Pool
@@ -6,32 +6,32 @@ require("dotenv").config()
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
-let pool 
-if (process.env.NODE_ENV == "develoment"){
-    pool = new Pool({
-        connectionString: process.env.DATEBASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-
-    })
-
-
-module.exports = {
-    async query(text, params){
-        try{
-            const res = await pool.query(text, params)
-            console.log("executed query", {text})
-            return res
-        } catch (error){
-            console.error("error in query", {text})
-            throw error
-        }
+let pool
+if (process.env.NODE_ENV == "development") {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
     },
+})
+
+// Added for troubleshooting queries
+// during development
+module.exports = {
+  async query(text, params) {
+    try {
+      const res = await pool.query(text, params)
+      console.log("executed query", { text })
+      return res
+    } catch (error) {
+      console.error("error in query", { text })
+      throw error
+    }
+  },
 }
 } else {
-    pool = new Pool({
-        connectionString: process.env.DATEBASE_URL,
-    })
-    module.exports = pool
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  })
+  module.exports = pool
 }
